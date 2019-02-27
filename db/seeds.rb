c31576d3f356174e43cdb8d14538b5fa5e876871
Ferdinand Nussbaum <ferdinand18.2n@gmail.com>
2019-02-27 17:36:12 +0100

    Yeahhhh

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese',
+    phone_number: '4455366636'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '445366636'
+  },
+  {
+    name:         'Mama',
+    address:      'Titt',
+    category:     'italian',
+    phone_number: '44536566636'
+  },
+  {
+    name:         'Papa',
+    address:      'Engelsberg',
+    category:     'italian',
+    phone_number: '4453666788636'
+  },
+  {
+    name:         'Maxi',
+    address:      'Engel',
+    category:     'french',
+    phone_number: '445366600036'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
